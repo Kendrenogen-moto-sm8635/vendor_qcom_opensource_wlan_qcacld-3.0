MotorolaMobilityLLC/vendor-qcom-opensource-wlan-qcacld-3.0

: Fix iwpriv panic
iwpriv tool uses extra parameter whereas Mot
    ioctl uses wrqu data structure. Need to differentiate
    between the two to resolve panic

Change-Id: Icb1c9e4f75ad1f89f43ff2be5fc7f551f3176851
Reviewed-on: https://gerrit.mot.com/1456946
SLTApproved: Slta Waiver
SME-Granted: SME Approvals Granted
Tested-by: Jira Key
Reviewed-by: Prashanth Chakravarthy <a19110@motorola.com>
Submit-Approved: Raja Phani K <a16821@motorola.com>

core/hdd/src/wlan_hdd_hostapd_wext.c
@@ -343,15 +343,33 @@ static __iw_softap_setparam(struct net_device *dev,
 {
 	struct hdd_adapter *adapter = (netdev_priv(dev));
 	mac_handle_t mac_handle;
-	int *value = (int *)extra;
-	int sub_cmd = value[0];
-	int set_value = value[1];
+        //BEGIN MOT a19110 IKDREL3KK-11113 Fix iwpriv panic
+        int *value;
+        int sub_cmd;
+        int set_value;
+        int *tmp = (int *) extra;
+       //END IKDREL3KK-11113
 	QDF_STATUS status;
 	int ret = 0;
 	struct hdd_context *hdd_ctx;
 	bool bval = false;
 
 	hdd_enter_dev(dev);
+        //BEGIN MOT a19110 IKDREL3KK-11113 Fix iwpriv panic to 8998
+        if (tmp[0] < 0 || tmp[0] > QCASAP_SET_TM_LEVEL) {
+               value = (int *)(wrqu->data.pointer);
+        } else {
+               value = (int *)extra;
+        }
+
+        sub_cmd = value[0];
+        set_value = value[1];
+
+        if (!adapter || !adapter->hdd_ctx) {
+               hdd_err("Either hostpad adapter is null or Hal ctx is null");
+               return -EINVAL;
+        }
+        //END IKDREL3KK-11113
 
 	hdd_ctx = WLAN_HDD_GET_CTX(adapter);
 	ret = wlan_hdd_validate_context(hdd_ctx);

Kendrenogen-moto-sm8635/vendor_qcom_opensource_wlan_qcacld-3.0

Reviewed-on: https://gerrit.mot.com/1736984
Submit-Approved: Jira Key
Reviewed-on: https://gerrit.mot.com/2026420
Reviewed-by: Ningkai Zhuang <zhuangnk1@lenovo.com>
Reviewed-by: Tao Sun <suntao2@motorola.com>
(cherry picked from commit 924106a8cd7f30f2b6eb364c25592ad3eec9d41e)
Reviewed-on: https://gerrit.mot.com/2372926
Reviewed-by: Rui Hu <hurui1@motorola.com>
Reviewed-on: https://gerrit.mot.com/2764173
Reviewed-by: Haichao Li <liuhc3@motorola.com>

core/hdd/src/wlan_hdd_hostapd_wext.c
@@ -361,9 +361,12 @@ static __iw_softap_setparam(struct net_device *dev,
 {
 	struct hdd_adapter *adapter = (netdev_priv(dev));
 	mac_handle_t mac_handle;
-	int *value = (int *)extra;
-	int sub_cmd = value[0];
-	int set_value = value[1];
+    //BEGIN MOT a19110 IKDREL3KK-11113 Fix iwpriv panic
+    int *value;
+    int sub_cmd;
+    int set_value;
+    int *tmp = (int *) extra;
+    //END IKDREL3KK-11113
 	QDF_STATUS status;
 	int ret = 0;
 	struct hdd_context *hdd_ctx;
@@ -372,6 +375,22 @@ static __iw_softap_setparam(struct net_device *dev,
 
 	hdd_enter_dev(dev);
 
+    //BEGIN MOT a19110 IKDREL3KK-11113 Fix iwpriv panic to 8998
+    if (tmp[0] < 0 || tmp[0] > QCSAP_SET_BTCOEX_LOW_RSSI_THRESHOLD) {
+           value = (int *)(wrqu->data.pointer);
+    } else {
+           value = (int *)extra;
+    }
+
+    sub_cmd = value[0];
+    set_value = value[1];
+
+    if (!adapter || !adapter->hdd_ctx) {
+           hdd_err("Either hostpad adapter is null or Hal ctx is null");
+           return -EINVAL;
+    }
+    //END IKDREL3KK-11113
+ 
 	hdd_ctx = WLAN_HDD_GET_CTX(adapter);
 	ret = wlan_hdd_validate_context(hdd_ctx);
 	if (0 != ret)